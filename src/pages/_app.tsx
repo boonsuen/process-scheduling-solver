4ce6746df949e129b2d50eb4dd249cc4e32d7a10
Boonsuen Oh <boonsuenoh@gmail.com>
2021-09-07 18:30:56 +0800

    Change script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,28 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href={img_favicon} />
-        <style dangerouslySetInnerHTML={{ 
-          __html: fontFaceRules 
-        }}></style>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: fontFaceRules,
+          }}
+        ></style>
         <script async src="https://cdn.splitbee.io/sb.js"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PR31CLCPW1"></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-PR31CLCPW1');`}
-        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PR31CLCPW1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PR31CLCPW1');`,
+          }}
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
