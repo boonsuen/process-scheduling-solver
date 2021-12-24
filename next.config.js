b9d644e25290866e42392e10239fea78b958fd37
Boonsuen Oh <boonsuenoh@gmail.com>
2021-12-24 23:43:46 +0800

    Next 12 upgrade

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  experimental: {
+    styledComponents: true
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
       {
